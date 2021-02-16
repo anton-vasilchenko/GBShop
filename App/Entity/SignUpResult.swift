25cd7ebf1feb216fb89c6cf83ef3c53a28224a2a
a.vasilchenko <anton.miet@gmail.com>
2021-02-16 13:24:22 +0300

    Auth + SignUp Factory

---
+++ App/Entity/SignUpResult.swift
@@ -0,0 +1,13 @@
+//
+//  SignUpResult.swift
+//  GBShop
+//
+//  Created by Антон Васильченко on 16.02.2021.
+//
+
+import Foundation
+
+struct SignUpResult: Codable {
+    let result: Int
+    let userMessage: String
+}
